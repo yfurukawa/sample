3968dda89c21014e5f460587bd225ebff47f57ec
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-09-03 20:56:09 +0900

    テキストファイルを読み込むように実装

--- textfiledao.cpp
+++ textfiledao.cpp
@@ -1,6 +1,43 @@
+
 #include "textfiledao.h"
 
-TextFileDao::TextFileDao()
+TextFileDao::TextFileDao() :
+    fileHandler_(nullptr),
+    inputStream_(nullptr)
 {
 
+}
+
+TextFileDao::~TextFileDao()
+{
+
+}
+
+void TextFileDao::open(QString fileName)
+{
+    fileHandler_ = new QFile(fileName);
+    if(!fileHandler_->open((QIODevice::ReadOnly)))
+    {
+        throw fileHandler_->errorString();
+    }
+    codec_ = QTextCodec::codecForName("UTF-8");
+    inputStream_ = new QTextStream(fileHandler_);
+    inputStream_->setCodec(codec_);
+}
+
+QString TextFileDao::readLine()
+{
+    QString readLine;
+
+    if(!inputStream_->atEnd())
+    {
+        readLine = inputStream_->readLine();
+    }
+
+    return readLine;
+}
+
+void TextFileDao::close()
+{
+    fileHandler_->close();
 }
--- textfiledao.h
+++ textfiledao.h
@@ -1,11 +1,24 @@
 #ifndef TEXTFILEDAO_H
 #define TEXTFILEDAO_H
 
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QTextCodec>
 
 class TextFileDao
 {
 public:
     TextFileDao();
+    virtual ~TextFileDao();
+    void open(QString fileName);
+    QString readLine();
+    void close();
+
+protected:
+    QFile* fileHandler_;
+    QTextStream* inputStream_;
+    QTextCodec* codec_;
 };
 
 #endif // TEXTFILEDAO_H
